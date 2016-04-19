527be063b796ae8a0c780f64ff255b99a95e5c24
slavam1 <mwm1955@gmail.com>
2016-04-19 10:43:27 +0000

    Created users table

--- db/migrate/20160418140602_create_users.rb
+++ db/migrate/20160418140602_create_users.rb
@@ -1,6 +1,20 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :picture
+      t.string :email
+      t.string :phone
+      t.string :website
+      t.string :fb_url
+      t.string :vk_url
+      t.string :ok_url
+      t.string :provider
+      t.string :uid
+      t.string :password_digest
+      t.string :code_token
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160418140602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.string   "picture"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "website"
+    t.string   "fb_url"
+    t.string   "vk_url"
+    t.string   "ok_url"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "password_digest"
+    t.string   "code_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
