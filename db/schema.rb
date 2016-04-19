3a62793d7a80317e1dc2a7fcd0249c81c247b03e
slavam1 <mwm1955@gmail.com>
2016-04-19 17:29:10 +0000

    Added index for unique Email user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418140602) do
+ActiveRecord::Schema.define(version: 20160419172136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,7 @@ ActiveRecord::Schema.define(version: 20160418140602) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
