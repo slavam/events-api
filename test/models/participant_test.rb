5206e44939abea80d09d712c96a5df2c751fdc19
slavam1 <mwm1955@gmail.com>
2016-04-22 17:36:56 +0000

    Added participant test

--- test/models/participant_test.rb
+++ test/models/participant_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @event = events(:feast)
+    @participant = @user.participants.build(event_id: @event.id, i_am_going: true)
+  end
+
+  test "should be valid" do
+    assert @participant.valid?
+  end
+
+  test "user id should be present" do
+    @participant.user_id = nil
+    assert_not @participant.valid?
+  end
 end
