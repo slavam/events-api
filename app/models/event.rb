262e0c0f4691a98804b23c0827aab49b5a4553a9
slavam1 <mwm1955@gmail.com>
2016-04-28 12:38:29 +0000

    Added user_id to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
   has_many :participants
   has_many :users, through: :participants
+  belongs_to :users
   validates :name, presence: true
   validates :date_start, presence: true
+  validates :user_id, presence: true
   
   def count_participants
     users.count
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421184027) do
+ActiveRecord::Schema.define(version: 20160428115347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20160421184027) do
     t.float    "lng"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "participants", force: :cascade do |t|
     t.integer  "user_id"
