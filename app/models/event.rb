84ae4cb62d5355605dabd989fcd57e764c24d26f
slavam1 <mwm1955@gmail.com>
2016-04-28 14:50:49 +0000

    Added test for user_id present

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   has_many :participants
   has_many :users, through: :participants
-  belongs_to :users
+  belongs_to :user
   validates :name, presence: true
   validates :date_start, presence: true
   validates :user_id, presence: true
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   def setup
-    @event = Event.new(name: "Example Event", date_start: "2017-01-01")
+    @user       = users(:michael)
+    @event = Event.new(name: "Example Event", date_start: "2017-01-01", user_id: @user.id)
   end
 
   test "should be valid" do
@@ -16,6 +17,11 @@ class EventTest < ActiveSupport::TestCase
 
   test "date_start should be present" do
     @event.date_start = nil
+    assert_not @event.valid?
+  end
+  
+  test "user_id should be present" do
+    @event.user_id = nil
     assert_not @event.valid?
   end
 end
