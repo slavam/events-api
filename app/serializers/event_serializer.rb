c5a33f85246968add58bf453022d760e822962c9
slavam1 <mwm1955@gmail.com>
2016-04-28 18:08:16 +0000

    Added author for event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :date_start, :date_end, :country,
         :city, :address, :lat, :lng
+  has_one :user, key: "author"
 end
