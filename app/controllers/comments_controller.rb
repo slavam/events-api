6b5368b1ae4531cf78674efcb9f401c14f751e19
slavam1 <mwm1955@gmail.com>
2016-06-13 08:59:33 +0000

    Changed route for comments 10

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,14 +8,14 @@ class CommentsController < ApplicationController
     per_page = params[:per_page]? params[:per_page].to_i : 25
     page = params[:page]? params[:page].to_i : 1
     comments = @event.comments
-    # cs = []
-    # comments.paginate(page: page, per_page: per_page).each do|c| 
-    #   cs << comment_to_hash(c)
-    # end
+    cs = []
+    comments.paginate(page: page, per_page: per_page).each do|c| 
+      cs << comment_to_hash(c)
+    end
     
-    # render json: {comments: cs, count: comments.count}    
+    render json: {comments: cs, count: comments.count}    
     # @comments = Comment.all
-    render json: comments
+    # render json: comments
   end
 
   # GET /comments/1
