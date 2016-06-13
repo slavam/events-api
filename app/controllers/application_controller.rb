3c85adf37145178f1dbde975d800a7f6875fde58
slavam1 <mwm1955@gmail.com>
2016-06-13 09:52:13 +0000

    Changed route for comments 22

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,8 +85,12 @@ class ApplicationController < ActionController::API
   end
 
   def comment_to_hash(comment)
+    if comment.recipient_id
+      recp = User.find(comment.recipient_id)
+      # rescue ActiveRecord::RecordNotFound
+    end
     {id: comment.id, event_id: comment.event_id, author: user_to_hash(comment.author),
-      recipient: "<#{comment.recipient_id}>", #(comment.recipient_id.nil? or comment.recipient_id.to_i < 1) ? nil : user_to_hash(User.find(comment.recipient_id)),
+      recipient: recp ? user_to_hash(recp) : nil,
       content: comment.content,
       created_at: comment.created_at.strftime('%Y-%m-%d %H:%M')
     }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   # belongs_to :user
   belongs_to :author, :class_name => 'User', foreign_key: 'user_id'
   belongs_to :event
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
 end
